ad15a68cbc872ef0330900c99ef6ab7eb706c830
Joshua <jsbyrd02@gmail.com>
2023-08-22 18:09:07 -0400

    Initialize spring boot application

---
+++ src/main/java/com/jsbyrd02/chessengineapi/ChessEngineApiApplication.java
@@ -0,0 +1,13 @@
+package com.jsbyrd02.chessengineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessEngineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessEngineApiApplication.class, args);
+	}
+
+}
